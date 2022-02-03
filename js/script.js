996d860cdf295247404e49e61fa1121fce9a490a
Lasse96 <lasse_strand_96@hotmail.com>
2022-02-03 12:24:26 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,61 @@ const games = [
         released: 2020
     }
 ];
+
+
+// Oppgave 1
+function printSeason(season) {
+    console.log(season);
+};
+
+printSeason("summer");
+
+// Oppgave 2
+function printNumber(firstNumber, secondNumber) {
+    console.log(firstNumber, secondNumber);
+};
+
+printNumber(1, 22);
+
+// Oppgave 3
+function addNumber(en, to, tre) {
+    const tot = en + to + tre;
+    return tot;
+};
+const totalhtml = document.querySelector(".total")
+const totalt = addNumber(4, 4, 1);
+totalhtml.innerHTML = totalt;
+
+// Oppgave 4
+function createGreeting(navn) {
+    const result = `Hello, my name is ${navn}`;
+    return result;
+}
+const namehtml = document.querySelector("#name");
+const navnet = createGreeting("Lasse");
+namehtml.innerHTML = navnet;
+
+// Oppgave 5
+function printListItems(sport) {
+    for (let i = 0; i < sport.length; i++) {
+        console.log(sport[i]);
+    }
+};
+printListItems(sports);
+
+// Oppgave 6
+function createGames(list) {
+    let tom = "";
+    for (let index = 0; index < list.length; index++) {
+        let year = "Uvisst";
+        if (list[index].released) {
+            year = list[index].released;
+        }
+        tom += `<div class ="game"><h3>${list[index].name}</h3>
+        <p>(${year})</p></div>`;
+    }
+    return tom;
+;}
+const html = createGames(games);
+const container = document.querySelector(".game-container");
+container.innerHTML = html;
